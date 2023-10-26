3bf06c8a73b76cfb74da0f0a81e00c211c844762
Pedro Santos <pedrosantosdevelop@gmail.com>
2023-10-26 18:15:23 -0300

    aes class

--- advanced_encription_standard/aes.py
+++ advanced_encription_standard/aes.py
@@ -1,74 +1,26 @@
 from dataclasses import dataclass
-from consts import S_BOX, INVERSE_S_BOX
-
-@dataclass
 class AES:
-    """Implementação do Advanced Encription Standard"""
-    key: bytes
-    text: bytes
+    def __init__(key: str, text: str, rounds: int):
+        # Precisamos dividir o texto em vários segmentos
+        self.key = bytearray.fromhex(key)
+        self.text = bytearray.fromhex(text)
+        self.rounds = rounds
     
+    def encript(self):
+        state = get_state()
+        pass        
 
-    def padding(text: str, block_size: int) -> bytes:
-        """
-        Dividir o texto em blocos de 16 bytes ou 128 bits.
-        """ 
-        # Transforma o texto em bytes
-        text = bytes(text)
-        # Calcula o numero de bytes
-        padding_size = block_size - (len(text) % block_size)
-        # Cria um byte com o tamanho correto
-        padding = bytes([padding_size] * padding_size)
-        # Adiciona o padding ao texto original
-        padded_text = text + padding
-    
-    return padded_text
+    def get_state(self):
+        pass
 
-    def key_expansion(key: str):
-        """
-        Recebe a chave e gera outras
-        de acordo com a tabela abaixo:
+if __name__ == "__main__":
 
-        | Tamanho da Chave (bits) | Quantidade de Rounds  |
-        |-------------------------|-----------------------|
-        | 128                     | 10                    |
-        | 192                     | 12                    |
-        | 256                     | 14                    |
-        """
-        keys = []
+    plaintext = bytearray.fromhex('00112233445566778899aabbccddeeff')
+    key = bytearray.fromhex('000102030405060708090a0b0c0d0e0f')
+    cipher = AES()
+    # expected_ciphertext = bytearray.fromhex('69c4e0d86a7b0430d8cdb78070b4c55a')
+    # ciphertext = aes_encryption(plaintext, key)
+    # recovered_plaintext = aes_decryption(ciphertext, key)
 
-        return keys
-
-    def add_round_key(key: str, text: str):
-        """
-        Recebe uma chave e um texto e cifra o texto
-    um exemplo seria a operação XOR bit a bit
-        """
-        ciphered_text = ""
-
-        return ciphered_text
-
-    def sub_bytes(cipher_text: str):
-        """
-        Substituir os bits por outros de acordo com algum padrão definido.
-        Esse documento mostra uma tabela de substituição:
-        https://www.ime.usp.br/~rt/cranalysis/AESSimplified
-        """
-        sub_bytes_result = ""
-
-        return sub_bytes_result
-
-    def shift_rows(cipher_text: str):
-        """
-        Fazer o shift das linhas
-        """
-        rows_shifted = ""
-
-        return rows_shifted
-
-    def mix_columns(cipher_text: str):
-        """
-        Fazer a mixagem das colunas
-        """
-        columns_mixed = ""
-
-        return columns_mixed
+    # assert (ciphertext == expected_ciphertext)
+    # assert (recovered_plaintext == plaintext)
